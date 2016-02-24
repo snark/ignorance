f8438fa83e4286e988545a93640bed8cc032f3ce
Steve Cook <scook@saymedia.com>
2016-02-24 18:41:38 -0500

    Negation flag test

--- tests/test_git_walk.py
+++ tests/test_git_walk.py
@@ -9,3 +9,16 @@ def test_basic_walk():
     for r, d, f in ignorance.git.walk(os.path.join(data_path, 'basic')):
         files.extend(f)
     assert files == ['.gitignore', 'bam', 'foo', 'ignored', 'zap']
+
+def test_overrides():
+    test_path = os.path.realpath(__file__)
+    data_path = os.path.realpath(os.path.join(test_path, '../data/git'))
+    files = []
+    for r, d, f in ignorance.git.walk(os.path.join(data_path, 'override')):
+        files.extend(f)
+    assert 'bar' in files
+    assert 'baz.tmpx' in files
+    assert 'override.tmp' in files
+    assert 'quux' in files
+    assert 'foo.tmp' not in files
+    assert 'order_counts.tmp' not in files
