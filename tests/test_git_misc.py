0e1b6faed7685c127ed247a6d5773b2eb35a212f
Steve Cook <scook@saymedia.com>
2016-03-05 17:23:25 -0500

    Adding quicky directory_only rule parsing test

--- tests/test_git_misc.py
+++ tests/test_git_misc.py
@@ -1,4 +1,5 @@
 import ignorance
+import itertools
 
 
 def test_rule_from_pattern_basics():
@@ -6,23 +7,34 @@ def test_rule_from_pattern_basics():
         'foo', 'ba[rz]', 'ba?', '*.foo', 'foo/bar', 'foo/*/bar', '/foo',
         '/foo/bar', '**/foo/bar', 'foo/**/bar', 'foo/bar/**'
     ]
-    negations = ['!' + pattern for pattern in to_test]
-    for pattern in to_test + negations:
-        rule = ignorance.git.rule_from_pattern(pattern)
-        assert rule.pattern == pattern
-        if pattern[0] == '!':
-            assert rule.negation is True
-            pattern = pattern[1:]
-        else:
-            assert rule.negation is False
-        expected_rexp = ignorance.utils.fnmatch_pathname_to_regex(pattern)
-        if '**' in pattern:
-            pass
-        elif '/' not in pattern:
-            assert rule.regex == expected_rexp
-            assert not rule.anchored
-        else:
-            if pattern[0] == '/':
-                expected_rexp = expected_rexp[1:]
-            assert rule.regex == '^' + expected_rexp
-            assert rule.anchored
+    for pattern in to_test:
+        for negation, dir_only in itertools.product(
+                (True, False), (True, False)):
+            if negation:
+                pattern = '!' + pattern
+            if dir_only:
+                pattern = pattern + '/'
+
+            rule = ignorance.git.rule_from_pattern(pattern)
+            assert rule.pattern == pattern
+            if pattern[0] == '!':
+                assert rule.negation is True
+                pattern = pattern[1:]
+            else:
+                assert rule.negation is False
+            if pattern[-1] == '/':
+                assert rule.directory_only is True
+                pattern = pattern[0:-1]
+            else:
+                assert rule.directory_only is False
+            expected_rexp = ignorance.utils.fnmatch_pathname_to_regex(pattern)
+            if '**' in pattern:
+                pass
+            elif '/' not in pattern:
+                assert rule.regex == expected_rexp
+                assert not rule.anchored
+            else:
+                if pattern[0] == '/':
+                    expected_rexp = expected_rexp[1:]
+                assert rule.regex == '^' + expected_rexp
+                assert rule.anchored
